17b18722613f58f6bf19cb3b1121c16323bb059b
Samuel <samuelbonillaf@javerianacali.edu.co>
2023-11-04 14:03:11 -0500

    Ejecutable

---
+++ dist/Clases/PowerUpVelocidad.py
@@ -0,0 +1,11 @@
+import pygame
+class PowerUpVelocidad(pygame.sprite.Sprite):
+  def __init__(self,Imagen):
+    super().__init__()
+    self.image = pygame.transform.scale(Imagen,(28,28))
+    self.image.set_colorkey((0,0,0))
+    self.rect = self.image.get_rect()
+
+
+
+
